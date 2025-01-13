7dfe0ca9244ba5aa7bf008898c70ab5bce9bfe4a
Mahadev <mohitemahadev121@gmail.com>
2025-01-13 13:12:43 +0530

    added secret variable

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const createDbPool = () => {
   try {
     const pool = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "project_management",
+      host: process.env.host,
+      user: process.env.user,
+      password: process.env.password,
+      database: process.env.database,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
